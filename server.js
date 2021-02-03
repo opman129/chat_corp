2bf57aee0297f1f902fa757dc2d8ddfb509fd69f
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-02-03 10:47:22 +0100

    setting up server.js

--- server.js
+++ server.js
@@ -2,3 +2,20 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+
+
+
+
+
+
+//Environment Variable for PORT);
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}`));
