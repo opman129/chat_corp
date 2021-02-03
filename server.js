f8c8ad9e69dbdb9ce6742696c5883b1b05db952b
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-02-03 12:04:23 +0100

    initialize socket.io

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
+const { Socket } = require('dgram');
 
 const app = express();
 const server = http.createServer(app);
@@ -15,20 +16,22 @@ const botName = 'ChatCorp Bot'
 
 //Run when client connect
 io.on('connection', socket => {
+    socket.on('joinRoom', ({ username, room }) =>{
     //Welcome message to single client/User
     socket.emit('message', formatMessage(botName, 'Welcome to ChatCorp!'));
 
     //Broadcast when a user connects to everyone except the client
     socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
-
-    //Runs when a user disconnects
-    socket.on('disconnect', () => {
-        io.emit('message', formatMessage(botName, 'A user has left the chat'));
     });
 
     //Listen for chatMessage
     socket.on('chatMessage', msg => {
         io.emit('message', formatMessage('USER', msg));
+    });
+
+    //Runs when a user disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
     });
 });
 
